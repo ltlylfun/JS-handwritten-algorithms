59b9f6a2581d95aed4b49251f212c1e2ef251530
ltlylfun <funjee126@qq.com>
2025-03-07 14:03:37 +0800

    添加合并重叠区间的函数实现及相关测试用例

---
+++ merge.js
@@ -0,0 +1,46 @@
+/*
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+
+ 
+
+示例 1：
+
+输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+输出：[[1,6],[8,10],[15,18]]
+解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+示例 2：
+
+输入：intervals = [[1,4],[4,5]]
+输出：[[1,5]]
+解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+*/
+
+function merge(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const res = [intervals[0]];
+  for (let i = 1; i < intervals.length; i++) {
+    let m = res.length - 1;
+    if (res[m][1] >= intervals[i][0]) {
+      res[m][1] = Math.max(res[m][1], intervals[i][1]);
+    } else {
+      res.push(intervals[i]);
+    }
+  }
+  return res;
+}
+
+// test
+console.log(
+  merge([
+    [1, 3],
+    [2, 6],
+    [8, 10],
+    [15, 18],
+  ])
+); // [[1, 6], [8, 10], [15, 18]]
+console.log(
+  merge([
+    [1, 4],
+    [4, 5],
+  ])
+); // [[1, 5]]
