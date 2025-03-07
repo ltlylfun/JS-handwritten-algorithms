87643498729f847a44e92c34b2307f6397951762
ltlylfun <funjee126@qq.com>
2025-03-07 14:16:05 +0800

    添加求解最大连续子数组和的函数实现及相关测试用例

---
+++ maxSubArray.js
@@ -0,0 +1,49 @@
+/*
+给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+子数组是数组中的一个连续部分。
+
+ 
+
+示例 1：
+
+输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出：6
+解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+示例 2：
+
+输入：nums = [1]
+输出：1
+示例 3：
+
+输入：nums = [5,4,-1,7,8]
+输出：23
+
+*/
+
+function maxSubArray(nums) {
+  let pre = 0;
+  let maxAns = nums[0];
+  nums.forEach((x) => {
+    pre = Math.max(pre + x, x);
+    maxAns = Math.max(maxAns, pre);
+  });
+  return maxAns;
+}
+
+function maxSubArray(nums) {
+  let ans = -Infinity;
+  let minPreSum = 0;
+  let preSum = 0;
+  for (const x of nums) {
+    preSum += x;
+    ans = Math.max(ans, preSum - minPreSum);
+    minPreSum = Math.min(minPreSum, preSum);
+  }
+  return ans;
+}
+// 测试用例
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
+console.log(maxSubArray([1])); // 1
+console.log(maxSubArray([5, 4, -1, 7, 8])); // 23
