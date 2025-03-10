64cadbaa2e4827c46da350b0e240e28c384c48db
ltlylfun <funjee126@qq.com>
2025-03-10 19:30:29 +0800

    添加计算二叉树最大深度的函数实现及相关测试用例

---
+++ maxDepth.js
@@ -0,0 +1,35 @@
+/*
+给定一个二叉树 root ，返回其最大深度。
+
+二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+
+示例 1：
+
+输入：root = [3,9,20,null,null,15,7]
+输出：3
+示例 2：
+
+输入：root = [1,null,2]
+输出：2
+*/
+
+//https://leetcode.cn/problems/maximum-depth-of-binary-tree?envType=study-plan-v2&envId=top-100-liked
+
+function maxDepth(root) {
+  if (root === null) return 0;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+}
+
+//test
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+const root = new TreeNode(
+  3,
+  new TreeNode(9),
+  new TreeNode(20, new TreeNode(15), new TreeNode(7))
+);
+console.log(maxDepth(root)); // 3
