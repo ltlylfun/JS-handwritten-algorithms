b4168af047076a936ddf4f6402188736b3b98bdc
ltlylfun <funjee126@qq.com>
2025-03-10 19:44:14 +0800

    添加检查二叉树是否轴对称的函数实现及相关测试用例

---
+++ isSymmetric.js
@@ -0,0 +1,39 @@
+/*
+给你一个二叉树的根节点 root ， 检查它是否轴对称。
+
+示例 1：
+
+输入：root = [1,2,2,3,4,4,3]
+输出：true
+示例 2：
+
+输入：root = [1,2,2,null,3,null,3]
+输出：false
+*/
+
+function isSymmetric(root) {
+  function check(left, right) {
+    if (left === null && right === null) return true;
+    if (left === null || right === null) return false;
+    return (
+      left.val === right.val &&
+      check(left.left, right.right) &&
+      check(left.right, right.left)
+    );
+  }
+  return check(root, root);
+}
+
+//test
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+const root = new TreeNode(
+  1,
+  new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+  new TreeNode(2, new TreeNode(4), new TreeNode(3))
+);
+console.log(isSymmetric(root)); // true
